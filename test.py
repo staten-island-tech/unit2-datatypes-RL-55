fac10b1c796e377a042867aa0769813fae3c1894
RL-55 <rickyl55@nycstudents.net>
2025-09-16 09:24:18 -0400

    Update test.py

---
+++ test.py
@@ -0,0 +1,39 @@
+#bill=input("Bill amount")
+#bill=float(bill)
+#tip=0.15*bill
+#int(tip)
+#total=bill+tip
+#print(total)
+
+sentence=input("Write a sentence.")
+y= sentence.split()
+
+print(y)
+
+#name=input("What's your name?")
+#print(name)
+#def add(x,y):
+#    return x*y
+#z=add(input("Number One "),input("Number Two "))
+#print(z)
+#five=int('5')
+#print(five+5)
+#name="Mason"
+#print(f"His name is {name}")
+#bill=3.14159
+#students=["Cadee","Mason","Andy"]
+#students.append("Alina")
+#print(students[2])
+#for student in students:
+#    print(student)
+#Boolean true or false
+#x=True
+#evaluations use double==
+#if x==True:
+#    print()
+#else:
+#    print()
+#y=False
+#if y==True:
+#elif y==None:
+#else y==False:
